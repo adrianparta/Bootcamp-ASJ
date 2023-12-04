fdd92c02cb9cafd6f3d266a88b9a01e1d1bf255f
unknown <apartarrieu@petersen.corp>
2023-12-04 10:09:56 -0300

    Agrego ejercicio 1

--- 20231204-Actividades-JS/main.js
+++ 20231204-Actividades-JS/main.js
@@ -1,5 +1,5 @@
-//Par impar switch
 
+//Par impar switch
 const num = parseInt(prompt("ingrese un número"));
 
 switch(num%2){
@@ -13,5 +13,31 @@ switch(num%2){
 
     default:
         alert("ingrese un numero valido");
+    break;
+}
+
+
+//ejercicio 1
+
+const text = prompt("ingrese una palabra:");
+
+switch(text){
+    case "casa":
+        alert("house");
+    break;
+    case "perro":
+        alert("dog");
+    break;
+    case "pelota":
+        alert("ball");
+    break;
+    case "árbol":
+        alert("tree");
+    break;
+    case "genio":
+        alert("genius");
+    break;
+    default:
+        alert("palabra incorrecta");
     break;
 }
