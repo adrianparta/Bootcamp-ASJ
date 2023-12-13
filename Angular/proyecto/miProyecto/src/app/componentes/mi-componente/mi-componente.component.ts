36b6ac0a971fca9f34f843aa8836a2094044eb32
unknown <apartarrieu@petersen.corp>
2023-12-13 11:28:46 -0300

    Actualización proyecto angular

--- Angular/proyecto/miProyecto/src/app/componentes/mi-componente/mi-componente.component.ts
+++ Angular/proyecto/miProyecto/src/app/componentes/mi-componente/mi-componente.component.ts
@@ -10,12 +10,12 @@ export class MiComponenteComponent {
     {
     nombre:"Iphone",
     descripcion:"Un telefono de apple",
-    imagen:"https://blog.hootsuite.com/wp-content/uploads/2023/09/5.jpeg"
+    imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3vEryHNY95cmBtjkkTOMrVgU12_cunZG0vewB6WFTQA2oC8j4MFjdkazTZcTMl9LSLkg&usqp=CAU"
     },
     {
-      nombre:"Iphone",
-      descripcion:"Un telefono de apple",
-      imagen:"https://blog.hootsuite.com/wp-content/uploads/2023/09/5.jpeg"
+      nombre:"Samsung",
+      descripcion:"Telefono cuadrado, grande",
+      imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6falbvKEBe0jeHEmXYljv8ZHt-Togx1pIztJ5xF0AB2bJgRJLVaNrLsED1ZbNwy4yReY&usqp=CAU"
     }
   ]
 }
