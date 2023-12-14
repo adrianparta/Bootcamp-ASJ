063227ad909d2d699107294451511c5d171fd78e
unknown <apartarrieu@petersen.corp>
2023-12-14 17:59:40 -0300

    Routing

--- Angular/Ejercicios/src/app/app-routing.module.ts
+++ Angular/Ejercicios/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Proyecto1Component } from './components/main/proyecto1/proyecto1.component';
+import { Proyecto2Component } from './components/main/proyecto2/proyecto2.component';
+
+const routes: Routes = [
+  {path: 'proyecto1', component: Proyecto1Component},
+  {path: 'proyecto2', component: Proyecto2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
